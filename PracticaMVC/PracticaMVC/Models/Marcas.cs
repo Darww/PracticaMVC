47234323a7060d84fef642b6eadd822c2fc2b1e0
Estudiante <Estudiante@PC01E30>
2025-03-07 17:51:20 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC/PracticaMVC/Models/Marcas.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PracticaMVC.Models
+{
+    public class Marcas
+    {
+
+        [Key]
+        public int Id_marcas { get; set; }
+        public string? nombre_marca { get; set; }
+        public string? estados {  get; set; }
+        
+    }
+}
---
+++ PracticaMVC/PracticaMVC/Models/equiposDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace PracticaMVC.Models
+{
+    public class equiposDbContext: DbContext
+    { 
+        public equiposDbContext(DbContextOptions<equiposDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Marcas> Marcas { get; set; }
+    }
+}
